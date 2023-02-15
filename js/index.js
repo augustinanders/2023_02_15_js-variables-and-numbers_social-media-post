8ed06c90b19d78e6689852196bfe8be4adedc45d
Augustin Anders <121796762+augustinanders@users.noreply.github.com>
2023-02-15 11:28:48 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,30 @@
+console.clear();
+
+/*
+1. Create the data for a small social media post. Create a const variable for each data point listed below.:
+	- a title
+	- a text
+	- the number of likes
+	- the user who created the post
+	- a boolean called isReported
+*/
+
+// --v-- write your code here --v--
+const title = "This is a beautiful title";
+const text =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+const likes = 20000;
+const creator = "Augustin";
+const isReported = true;
+// --^-- write your code here --^--
+
+/*
+2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
+*/
+
+// --v-- write your code here --v--
+console.log(title);
+console.log("creator: " + creator);
+console.log(text);
+console.log(isReported);
+// --^-- write your code here --^--
